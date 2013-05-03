e2b38fbec19a2cce58a9f8feb6e94018f200bd89
Stephan Preibisch <stephan.preibisch@gmx.de>
2013-05-03 14:35:59 -0500

    the readables extend only readable, not each other ... mmh ...

--- net/imglib3/type/try4/ByteReadable.java
+++ net/imglib3/type/try4/ByteReadable.java
@@ -1,6 +1,6 @@
 package net.imglib3.type.try4;
 
-public interface ByteReadable< R extends Byte > extends IntReadable< R >
+public interface ByteReadable< R extends Byte > extends Readable< R >
 {
 	//byte getByte();
 }
